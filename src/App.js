a279591177a1e5dac4e31e21be204ea4180727fd
Amrit Dalal <dalalamrit@gmail.com>
2022-05-15 18:59:53 +0530

    removed routing to about for deploying using github

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import About from './components/About';
+// import About from './components/About';
 import React, { useState } from 'react';
 import Alert from './components/Alert';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route
+// } from "react-router-dom";
 
  
 function App() {
@@ -39,21 +39,21 @@ function App() {
   }
   return (
     <>
-    <Router>
+    {/* <Router> */}
     <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} key={new Date()} />
     <Alert alert={alert}/>
     <div className="container my-3">
-          <Routes>
+          {/* <Routes>
             <Route exact path="/about" element={<About/>}>
-            </Route>
-            <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode}/>}>
-            
-            </Route>
-          </Routes>
+            </Route> */}
+            {/* <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode}/>}> */}
+            <TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode}/>
+            {/* </Route>
+          </Routes> */}
 
           {/* <About/> */}
         </div>
-      </Router>
+      {/* </Router> */}
     </>
   );
 }
